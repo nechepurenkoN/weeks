c8316bddf171a3b7f0a75f9888ada65b42cf7c77
nechepurenkoN <rufflerock@gmail.com>
2024-01-04 19:19:12 +0100

    add tailwind and react-select

--- src/CountrySelector.js
+++ src/CountrySelector.js
@@ -1,15 +1,21 @@
 import { countryToSexToAge } from './data';
+import Select from 'react-select'
 
 export default function CountrySelector({ setCountryState }) {
-    
+
     const choice = Object.keys(countryToSexToAge)
-    
+
     return (
-        <select onChange={(ev) => setCountryState(ev.target.value)}>
-            {choice.map(country => (
-                <option key={country} value={country}>{country}</option>
-            ))}
-        </select>
+        <Select
+            options={choice.map(country => { return { label: country, value: country } })}
+            formatOptionLabel={country => (
+                <div className='country-option'>
+                    <img src={countryToSexToAge[country.value].flag} style={{ display: "inline" }} />
+                    <span>{country.label}</span>
+                </div>
+            )}
+            onChange={(option, action) => setCountryState(option.value)}
+        />
     )
 
 }
